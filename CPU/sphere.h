0fe44559b4ab81b8c17f5e5303ac4830521176e8
Finley Harrison <147407110+not-finley@users.noreply.github.com>
2024-05-15 14:56:23 -0400

    Update sphere.h

--- CPU/sphere.h
+++ CPU/sphere.h
@@ -8,12 +8,34 @@ class material;
 class sphere: public hitable {
 public:
     sphere();
-    sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m) {};
+	// Stationary Sphere
+    sphere(const vec3& center, float radius, material *mat)
+    : center1(center), radius(fmax(0,radius)), mat_ptr(mat), is_moving(false) {}
+
+    // Moving Sphere
+    sphere(const vec3& center1, const vec3& center2, float radius, material* mat)
+	: center1(center1), radius(fmax(0,radius)), mat_ptr(mat), is_moving(true)
+    {
+        center_vec = center2 - center1;
+    }
+	
+    // sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m) {};
     virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
     vec3 center; float radius; material *mat_ptr;
+
+	vec3 center1;
+	bool is_moving;
+    vec3 center_vec;
+
+    vec3 sphere_center(double time) const {
+        // Linearly interpolate from center1 to center2 according to time, where t=0 yields
+        // center1, and t=1 yields center2.
+        return center1 + time*center_vec;
+    }
 };
 
 bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+	vec3 center = is_moving ? sphere_center(r.time()) : center1;
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(),r.direction());
 	float b = dot(oc, r.direction());
